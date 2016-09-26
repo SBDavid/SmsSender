d1c2688c914865651ef2ce50bfdff62d72ab9376
ssy <305679547@qq.com>
2016-09-26 16:34:58 +0800

    add new logger type: 1)debug 2)exception

--- SmsFactory-Maven/src/main/resources/david/tang/sms/logger/ConsoleLogger.java
+++ SmsFactory-Maven/src/main/resources/david/tang/sms/logger/ConsoleLogger.java
@@ -11,15 +11,37 @@ public class ConsoleLogger implements NormalLoggerI {
 	}
 
 	@Override
-	public boolean info(String info) {
-		System.out.println(info);
-		return false;
+	public boolean info(String msg) {
+		System.out.println(msg);
+		return true;
 	}
 
 	@Override
-	public boolean error(String error) {
-		System.out.println(error);
-		return false;
+	public boolean error(String msg) {
+		System.out.println(msg);
+		return true;
 	}
 
+	@Override
+	public boolean debug(String msg) {
+		System.out.println(msg);
+		return true;
+	}
+
+	@Override
+	public boolean exception(Throwable ex) {
+		System.out.println(errorException(ex));
+		return true;
+	}
+	
+	private String errorException(Throwable e) {
+		StackTraceElement[] ste = e.getStackTrace();
+		StringBuffer sb = new StringBuffer();
+		sb.append(e.toString() + "\n");
+		
+		for (int i = 0; i < ste.length; i++) {
+			sb.append("    at "+ste[i].toString() + "\n");
+		}
+		return sb.toString();
+	}
 }
--- SmsFactory-Maven/src/main/resources/david/tang/sms/logger/NormalLoggerI.java
+++ SmsFactory-Maven/src/main/resources/david/tang/sms/logger/NormalLoggerI.java
@@ -3,7 +3,11 @@ package main.resources.david.tang.sms.logger;
 public interface NormalLoggerI {
 	boolean sendRecord(String type, String number, String content, String status);
 	
-	boolean info(String info);
+	boolean info(String msg);
 	
-	boolean error(String error);
+	boolean error(String msg);
+	
+	boolean debug(String msg);
+	
+	boolean exception(Throwable ex);
 }
